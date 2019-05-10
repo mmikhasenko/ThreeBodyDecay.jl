d0a9005e4c346590535424a9bc5bd676a3506444
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2019-05-10 17:22:37 +0200

    Files generated by PkgTemplates

---
+++ src/ThreeBodyDecay.jl
@@ -0,0 +1,5 @@
+module ThreeBodyDecay
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ThreeBodyDecay
+using Test
+
+@testset "ThreeBodyDecay.jl" begin
+    # Write your own tests here.
+end
