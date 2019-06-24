6d2a775249582acf08ff309a08726197778d662d
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2019-06-24 11:56:00 +0200

    Update the input format. Couplings come separaterly

--- src/half_to_half_zero_zero.jl
+++ src/half_to_half_zero_zero.jl
@@ -1,19 +1,20 @@
 
+export RkΛsτ
 # couplings
 # specific for 1/2 -> (1/2 0 0) final state
 #
-R1Λsτ(two_λp,two_Λ,two_τ,chains) =
-    sum(v*HelicityRecoupling_doublearg([(two_j(chain),two_τ,1,two_λp)=>(two_J(chain),two_L(chain),two_S(chain))]) for (chain,v) in chains)
-R2Λsτ(two_λp,two_Λ,two_τ,chains) =
+R1Λsτ(two_λp,two_Λ,two_τ,chains,W) =
+    sum(v*HelicityRecoupling_doublearg([(two_j(chain),two_τ,1,two_λp)=>(two_J(chain),two_L(chain),two_S(chain))]) for (chain,v) in zip(chains,W))
+R2Λsτ(two_λp,two_Λ,two_τ,chains,W) =
     sum(v*HelicityRecoupling_doublearg([(two_j(chain),two_τ,0,0)=>(two_J(chain),two_L(chain),two_S(chain)),
-                                        (0,0,1,two_λp)=>(two_j(chain),two_l(chain),two_s(chain))]) for (chain,v) in chains)
-R3Λsτ(two_λp,two_Λ,two_τ,chains) =
+                                        (0,0,1,two_λp)=>(two_j(chain),two_l(chain),two_s(chain))]) for (chain,v) in zip(chains,W))
+R3Λsτ(two_λp,two_Λ,two_τ,chains,W) =
     sum(v*HelicityRecoupling_doublearg([(two_j(chain),two_τ,0,0)=>(two_J(chain),two_L(chain),two_S(chain)),
-                                        (1,two_λp,0,0)=>(two_j(chain),two_l(chain),two_s(chain))]) for (chain,v) in chains)
-function RkΛsτ(k,two_λp,two_Λ,two_τ,chains)
-    (k==1) && return R1Λsτ(two_λp,two_Λ,two_τ,chains)
-    (k==2) && return R2Λsτ(two_λp,two_Λ,two_τ,chains)
-    (k==3) && return R3Λsτ(two_λp,two_Λ,two_τ,chains)
+                                        (1,two_λp,0,0)=>(two_j(chain),two_l(chain),two_s(chain))]) for (chain,v) in zip(chains,W))
+function RkΛsτ(k,two_λp,two_Λ,two_τ,chains,W)
+    (k==1) && return R1Λsτ(two_λp,two_Λ,two_τ,chains,W)
+    (k==2) && return R2Λsτ(two_λp,two_Λ,two_τ,chains,W)
+    (k==3) && return R3Λsτ(two_λp,two_Λ,two_τ,chains,W)
     return 0.0im
 end
 
@@ -52,7 +53,7 @@ Full amplitude
  - specific for 1/2 -> (1/2 0 0) final state
  - with reference to p <--o--> K*
 ```
-function amp_b2bzz(two_λ,two_Λ,σ3,σ1,Cls,tbs)
+function amp_b2bzz(two_λ,two_Λ,σ3,σ1,CS,tbs,Cs)
     σ2 = gσ2(σ3,σ1,tbs)
     # Wigner rotations
     D1 = [two_λ==two_λp ? 1.0 : 0.0 for two_λp=-1:2:1]
@@ -66,12 +67,14 @@ function amp_b2bzz(two_λ,two_Λ,σ3,σ1,Cls,tbs)
     Ds = (D1,D2,D3)
     #
     val = 0.000im
-    for (k,ξ,chains) in Cls
+    for (cs,W) in zip(CS, Cs) # coupling scheme and couplings
+        (k,ξ,chains) = cs
         length(chains) == 0 && continue
         decay_amp = 0.0im
-        for two_τ in -two_j(chains[1][1]):2:two_j(chains[1][1]), two_λp = -1:2:1
-            decay_amp += RkΛsτ(k,two_λp,two_Λ,two_τ,chains) *
-                    ZkΛsτ(k,two_λp,two_Λ,two_j(chains[1][1]),two_τ,σ3,σ1,tbs) *
+        for two_τ in -two_j(chains[1]):2:two_j(chains[1]), two_λp = -1:2:1
+            Wc = [W[i,1] + 1im * W[i,2] for i in 1:size(W,1)]
+            decay_amp += RkΛsτ(k,two_λp,two_Λ,two_τ,chains, Wc) *
+                    ZkΛsτ(k,two_λp,two_Λ,two_j(chains[1]),two_τ,σ3,σ1,tbs) *
                     Ds[k][div(two_λp+3,2)]
         end
         val += amp(σs[k],ξ) * decay_amp
@@ -84,9 +87,9 @@ Squared amplitude summed over polarization of initial and final state
  - specific for 1/2 -> (1/2 0 0) final state
  - with reference to p <--o--> K*
 ```
-function rate_b2bzz(σ3,σ1,Cksl,tbs)
+function rate_b2bzz(σ3,σ1,CS,tbs,Cs)
     return sum(
-        abs2(amp_b2bzz(two_λ,two_Λ,σ3,σ1,Cksl,tbs))
+        abs2(amp_b2bzz(two_λ,two_Λ,σ3,σ1,CS,tbs,Cs))
             for two_λ in -1:2:1
                 for two_Λ in -1:2:1)
 end
@@ -96,10 +99,10 @@ A(C1)A*(C1) summed over polarization of initial and final state
  - specific for 1/2 -> (1/2 0 0) final state
  - with reference to p <--o--> K*
 ```
-function rateCC_b2bzz(σ3,σ1,Cls1,Cls2,tbs)
+function rateCC_b2bzz(σ3,σ1,CS,tbs,Cs1,Cs2)
     return sum(
-        amp_b2bzz(two_λ,two_Λ,σ3,σ1,Cls1,tbs) *
-            conj(amp_b2bzz(two_λ,two_Λ,σ3,σ1,Cls2,tbs))
+        amp_b2bzz(two_λ,two_Λ,σ3,σ1,CS,tbs,Cs1) *
+            conj(amp_b2bzz(two_λ,two_Λ,σ3,σ1,CS,tbs,Cs2))
             for two_λ in -1:2:1
                 for two_Λ in -1:2:1)
 end
@@ -109,10 +112,10 @@ A(Λ1)A*(Λ2) summed over the final-state polarization
  - specific for 1/2 -> (1/2 0 0) final state
  - with reference to p <--o--> K*
 ```
-function rateΛΛ_b2bzz(two_Λ1,two_Λ2,σ3,σ1,Cls,tbs)
+function rateΛΛ_b2bzz(two_Λ1,two_Λ2,σ3,σ1,CS,tbs)
     return sum(
-        amp_b2bzz(two_λ,two_Λ1,σ3,σ1,Cls,tbs) *
-            conj(amp_b2bzz(two_λ,two_Λ2,σ3,σ1,Cls,tbs))
+        amp_b2bzz(two_λ,two_Λ1,σ3,σ1,CS,tbs,Cs) *
+            conj(amp_b2bzz(two_λ,two_Λ2,σ3,σ1,CS,tbs,Cs))
             for two_λ in -1:2:1)
 end
 
@@ -128,8 +131,8 @@ Gives a vector of the the polarization sensetivity
  - specific for 1/2 -> (1/2 0 0) final state
  - with reference to p <--o--> K*
 ```
-function polSens_b2bzz(σ3,σ1,Cls,tbs)
-    M = [rateΛΛ_b2bzz(two_Λ1,two_Λ2,σ3,σ1,Cls,tbs) for two_Λ1=-1:2:1, two_Λ2=-1:2:1]
+function polSens_b2bzz(σ3,σ1,CS,tbs,Cs)
+    M = [rateΛΛ_b2bzz(two_Λ1,two_Λ2,σ3,σ1,CS,tbs,Cs) for two_Λ1=-1:2:1, two_Λ2=-1:2:1]
     P = polarization_vector(M)
     return P
 end
@@ -139,49 +142,49 @@ Gives a vector of the the polarization sensetivity
  - specific for 1/2 -> (1/2 0 0) final state
  - with reference to p <--o--> K*
 ```
-function polSens_b2bzz(Cls,tbs; gridN::Int = 100)
+function polSens_b2bzz(CS,tbs, Cs; gridN::Int = 100)
     σ1v = LinRange(tbs.mthsq[1],tbs.sthsq[1],gridN)
     σ3v = LinRange(tbs.mthsq[3],tbs.sthsq[3],gridN)
     #
-    M11 = sum(Kibble31(σ3,σ1,tbs) > 0.0 ? 0.0 : rateΛΛ_b2bzz(-1,-1,σ3,σ1,Cls,tbs) for σ3 in σ3v, σ1 in σ1v)
-    M12 = sum(Kibble31(σ3,σ1,tbs) > 0.0 ? 0.0 : rateΛΛ_b2bzz( 1,-1,σ3,σ1,Cls,tbs) for σ3 in σ3v, σ1 in σ1v)
-    M22 = sum(Kibble31(σ3,σ1,tbs) > 0.0 ? 0.0 : rateΛΛ_b2bzz( 1, 1,σ3,σ1,Cls,tbs) for σ3 in σ3v, σ1 in σ1v)
+    M11 = sum(Kibble31(σ3,σ1,tbs) > 0.0 ? 0.0 : rateΛΛ_b2bzz(-1,-1,σ3,σ1,CS,tbs,Cs) for σ3 in σ3v, σ1 in σ1v)
+    M12 = sum(Kibble31(σ3,σ1,tbs) > 0.0 ? 0.0 : rateΛΛ_b2bzz( 1,-1,σ3,σ1,CS,tbs,Cs) for σ3 in σ3v, σ1 in σ1v)
+    M22 = sum(Kibble31(σ3,σ1,tbs) > 0.0 ? 0.0 : rateΛΛ_b2bzz( 1, 1,σ3,σ1,CS,tbs,Cs) for σ3 in σ3v, σ1 in σ1v)
     #
     P = polarization_vector([M11 M12; conj(M12) M22])
     return P
 end
 
 
-#############################################################################
-
-```
-Get complex couplings of the decay chain
-
-Comment: for the b2bzz, the decay chain set up is transfered to the amplitude
-in a specific form
-    Vector{Tuple{Int,Lineshape,Vector{Pair{twochain,Complex{Float64}}}}}
-```
-function get_couplings(Cls)
-    couplins = vcat(map(x->map(y->y[2],x[3]),Cls)...)
-    return couplins
-end
-
-```
-Set the complex couplings to the decay chain
-
-Comment: for the b2bzz, the decay chain set up is transfered to the amplitude
-in a specific form
-    Vector{Tuple{Int,Lineshape,Vector{Pair{twochain,Complex{Float64}}}}}
-```
-function set_couplings(Cls, p)
-    new_Cls = Vector{Tuple{Int,Lineshape,Vector{Pair{twochain,Complex{Float64}}}}}(undef,0);
-    ip=1;
-    for (k,f,couplings) in Cls
-        new_couplings = Vector{Pair{twochain,Complex{Float64}}}(undef,0)
-        for (chain,v) in couplings
-            push!(new_couplings,chain=>p[ip]); ip+=1
-        end
-        push!(new_Cls,(k,f,new_couplings))
-    end
-    return new_Cls
-end
+# #############################################################################
+#
+# ```
+# Get complex couplings of the decay chain
+#
+# Comment: for the b2bzz, the decay chain set up is transfered to the amplitude
+# in a specific form
+#     Vector{Tuple{Int,Lineshape,Vector{Pair{twochain,Number}}}}
+# ```
+# function get_couplings(Cls)
+#     couplins = vcat(map(x->map(y->y[2],x[3]),Cls)...)
+#     return couplins
+# end
+#
+# ```
+# Set the complex couplings to the decay chain
+#
+# Comment: for the b2bzz, the decay chain set up is transfered to the amplitude
+# in a specific form
+#     Vector{Tuple{Int,Lineshape,Vector{Pair{twochain,Number}}}}
+# ```
+# function set_couplings(Cls, p)
+#     new_Cls = Vector{Tuple{Int,Lineshape,Vector{Pair{twochain,Number}}}}(undef,0);
+#     ip=1;
+#     for (k,f,couplings) in Cls
+#         new_couplings = Vector{Pair{twochain,Number}}(undef,0)
+#         for (chain,v) in couplings
+#             push!(new_couplings,chain=>p[ip]); ip+=1
+#         end
+#         push!(new_Cls,(k,f,new_couplings))
+#     end
+#     return new_Cls
+# end
