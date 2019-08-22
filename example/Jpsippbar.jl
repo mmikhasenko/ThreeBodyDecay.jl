89f6cf95f6a28bdbff5d94d8471bb86dd44f708e
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2019-08-22 13:24:24 +0200

    Added a few examples:  - pentaquarks and  - Lc with resonances

---
+++ example/Jpsippbar.jl
@@ -0,0 +1,14 @@
+using ThreeBodyDecay
+using Plots
+
+const B2psippbar = let mB = 5.27932
+    ThreeBodySystem(mB,0.938,3.09,0.938)
+end
+
+σ3MC, σ1MC = flatDalitzPlotSample31(B2psippbar)
+
+let
+    histogram2d(σ3MC, σ1MC, bins=50)
+    hline!([4.312].^2, lab="")
+    vline!([4.312].^2, lab="")
+end
---
+++ example/Lb2JpsipK.jl
@@ -0,0 +1,70 @@
+using ThreeBodyDecay
+using Plots
+
+
+H1(two_τ,two_j,two_L) = ClGd(two_L,0,two_j,two_τ,1,two_τ)
+H2(two_μ,two_λ,two_j,two_s,two_l) = ClGd(2,two_μ,1,         -two_λ,two_s,two_μ-two_λ)*
+                              ClGd(two_l,0,two_s,two_μ-two_λ,two_j,two_μ-two_λ)
+#
+I(cosθ,two_j,two_s,two_l,two_L) = sum(abs2,
+    H1(two_τ,two_j,two_L)*
+    wignerd_doublearg(two_j,two_τ,two_μ-two_λ,cosθ)*
+    H2(two_μ,two_λ,two_j,two_s,two_l) for two_μ=-2:2:2, two_λ=-1:2:1, two_τ=(-two_j):2:two_j)
+
+let jp = (3,"-")
+    ( :LS=>posibleLS((1,"+"),jp,(0,"-")),
+      :ls=>posibleLS(jp,(2,"-"),(1,"+")) )
+end
+
+let
+    two_j,two_s,two_l,two_L = 3,3,2,2
+    plot(cosθ->I(cosθ,two_j,two_s,two_l,two_L),-1,1)
+end
+############################################################
+
+const Lb2JpK = let mLb = 5.62, mJψ = 3.09, mp=0.938, mK = 0.49367
+    ThreeBodySystem(mLb,mJψ,mp,mK)
+end
+
+function plot_dalitz_with_projections(f=(σ3,σ1)->1.0)
+    # layout = @layout [a{0.8h}; grid(1,2)]
+    layout = @layout [a{0.65w,0.7h} b; c _]
+    plot(layout=layout, size=(800,600), link=:both)
+    #
+    σ1v = LinRange(Lb2JpK.mthsq[1],Lb2JpK.sthsq[1], 202)
+    σ3v = LinRange(Lb2JpK.mthsq[3],Lb2JpK.sthsq[3], 200)
+    cal = [Kibble31(σ3,σ1,Lb2JpK) < 0 ? f(σ3,σ1) : NaN for σ3 in σ3v, σ1 in σ1v]
+    heatmap!(σ1v, σ3v, cal, c=:viridis, colorbar=false, sp=1,
+        ylab="m[J/psi p] (GeV^2)", xlab="m[p K] (GeV^2)")
+    #
+    calz = map(z->isnan(z) ? 0.0 : z, cal)
+    plot!(sum(calz, dims=2)[:,1], σ3v, lab="", xaxis=false, l=(2,:black), sp=2)
+    plot!(σ1v, sum(calz, dims=1)[1,:], lab="", yaxis=false, l=(2,:black), sp=3)
+end
+
+############################################################
+
+function A(σ3,σ1, CS, Cs)
+    σ = [σ1, 0.0, σ3]
+    sum(c*amp(σ[ch[1]], ch[2])  for (ch,c) in zip(CS,Cs))
+end
+
+I(σ3,σ1, CS, Cs) = abs2(A(σ3,σ1, CS, Cs))
+
+Λ1405  = BreitWigner(1.405,   0.090)
+Λ1520  = BreitWigner(1.5195,  0.0156)
+Λ1690  = BreitWigner(1.685,   0.050)
+Λ1810  = BreitWigner(1.80,    0.090)
+
+model = [(1,Λ1405),
+ (1,Λ1520),
+ (1,BreitWigner(1.6,0.2)),
+ (1,Λ1690),
+ (1,Λ1810),
+ (3,BreitWigner(4.45,0.06))]
+
+let
+    plot_dalitz_with_projections((σ3,σ1)->I(σ3,σ1,
+        model,
+        [0.9, 1.0, 0.7, 0.2, 0.3, 0.2im]))
+end
