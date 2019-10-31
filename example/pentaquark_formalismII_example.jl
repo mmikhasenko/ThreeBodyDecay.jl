eaf02c3f53f729b5dc6399ed9ce685390032108e
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2019-10-31 10:50:28 +0100

    fixed default argument

--- example/pentaquark_formalismII_example.jl
+++ example/pentaquark_formalismII_example.jl
@@ -30,7 +30,7 @@ end
 
 # amplitude squared
 Asq_summed(z;two_s=3,two_LS=(4,3),two_l=4) =
-    sum(abs2(A(z,two_λs;two_s=3,two_LS=two_LS,two_l=two_l))
+    sum(abs2(A(z,two_λs;two_s=two_s,two_LS=two_LS,two_l=two_l))
         for two_λs in Iterators.product(-tbs.two_js[1]:2:tbs.two_js[1],
                                         -tbs.two_js[2]:2:tbs.two_js[2],
                                         -tbs.two_js[3]:2:tbs.two_js[3],
@@ -43,3 +43,6 @@ using Plots
 plot(z->Asq_summed(z),-1,1, lab="", ylim=(0,1))
 # P-wave
 plot(z->Asq_summed(z;two_s=3,two_LS=(2,3),two_l=4),-1,1, lab="", ylim=(0,1))
+# s = 1/2, LS=(0,1/2), l=0, angle dependence is constant again
+plot(z->Asq_summed(z;two_s=1,two_LS=(0,1),two_l=0)+1e-5*z,-1,1, lab="", ylim=(0,3))
+#
