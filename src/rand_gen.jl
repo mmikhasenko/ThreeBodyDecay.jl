809179e5246a9fa3fea051606d9fa17be1eb53bf
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2019-11-10 19:23:49 +0100

    Minor fixes to pass tests

--- src/rand_gen.jl
+++ src/rand_gen.jl
@@ -9,7 +9,8 @@
 
 
 function flatDalitzPlotSample31(tbs; Nev::Int=10000, σ3bins::Int=500)
-    density = getbinned1dDensity(σ1->sqrt(λ(σ1,tbs.msq[2],tbs.msq[3])*λ(σ1,s(tbs),tbs.msq[1]))/σ1, (tbs.mthsq[1],tbs.sthsq[1]), σ3bins)
+    s = tbs.msq[4]
+    density = getbinned1dDensity(σ1->sqrt(λ(σ1,tbs.msq[2],tbs.msq[3])*λ(σ1,s,tbs.msq[1]))/σ1, (tbs.mthsq[1],tbs.sthsq[1]), σ3bins)
     σ1 = [rand(density) for _ in 1:Nev]
     σ3 = [σ3of1(2*rand()-1,σ,tbs.msq) for σ in σ1]
     return (σ3, σ1)
--- test/wigner_permutations.jl
+++ test/wigner_permutations.jl
@@ -8,20 +8,20 @@ using Test
     dpp = randomPoint(tbs)
 
     # (23) cosζ31_for1 = cosζ23_for1
-    @test cosζk1_for1(2,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],s(tbs)]) ==
-          cosζk1_for1(3,[dpp.σs[1],dpp.σs[3],dpp.σs[2]],[tbs.msq[1],tbs.msq[3],tbs.msq[2],s(tbs)])
+    @test cosζk1_for1(2,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],tbs.msq[4]]) ==
+          cosζk1_for1(3,[dpp.σs[1],dpp.σs[3],dpp.σs[2]],[tbs.msq[1],tbs.msq[3],tbs.msq[2],tbs.msq[4]])
     # (31) cosζ21_for2 = cosζ32_for2
-    @test cosζk2_for2(3,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],s(tbs)]) ==
-          cosζk2_for2(1,[dpp.σs[3],dpp.σs[2],dpp.σs[1]],[tbs.msq[3],tbs.msq[2],tbs.msq[1],s(tbs)])
+    @test cosζk2_for2(3,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],tbs.msq[4]]) ==
+          cosζk2_for2(1,[dpp.σs[3],dpp.σs[2],dpp.σs[1]],[tbs.msq[3],tbs.msq[2],tbs.msq[1],tbs.msq[4]])
     # (12) cosζ32_for3 = cosζ13_for3
-    @test cosζk3_for3(1,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],s(tbs)]) ==
-          cosζk3_for3(2,[dpp.σs[2],dpp.σs[1],dpp.σs[3]],[tbs.msq[2],tbs.msq[1],tbs.msq[3],s(tbs)])
+    @test cosζk3_for3(1,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],tbs.msq[4]]) ==
+          cosζk3_for3(2,[dpp.σs[2],dpp.σs[1],dpp.σs[3]],[tbs.msq[2],tbs.msq[1],tbs.msq[3],tbs.msq[4]])
 
     # (123) cosζ31_for1 = cosζ21_for2
-    @test cosζk2_for2(1,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],s(tbs)]) ==
-          cosζk1_for1(3,[dpp.σs[2],dpp.σs[3],dpp.σs[1]],[tbs.msq[2],tbs.msq[3],tbs.msq[1],s(tbs)])
+    @test cosζk2_for2(1,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],tbs.msq[4]]) ==
+          cosζk1_for1(3,[dpp.σs[2],dpp.σs[3],dpp.σs[1]],[tbs.msq[2],tbs.msq[3],tbs.msq[1],tbs.msq[4]])
     # (123)^2 cosζ31_for1 = cosζ23_for3
-    @test cosζk3_for3(2,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],s(tbs)]) ==
-          cosζk1_for1(3,[dpp.σs[3],dpp.σs[1],dpp.σs[2]],[tbs.msq[3],tbs.msq[1],tbs.msq[2],s(tbs)])
+    @test cosζk3_for3(2,[dpp.σs[1],dpp.σs[2],dpp.σs[3]],[tbs.msq[1],tbs.msq[2],tbs.msq[3],tbs.msq[4]]) ==
+          cosζk1_for1(3,[dpp.σs[3],dpp.σs[1],dpp.σs[2]],[tbs.msq[3],tbs.msq[1],tbs.msq[2],tbs.msq[4]])
     #
 end
