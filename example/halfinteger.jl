01be61b5ab56f191e09f28c97f9e64755efe8bee
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2022-03-07 21:39:39 +0100

    renamed tests

---
+++ example/halfinteger.jl
@@ -0,0 +1,3 @@
+using ThreeBodyDecay
+
+ThreeBodySpins(1//2, 1//2, 3//2, 3//2) |> x2
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,10 @@ using ThreeBodyDecay
 
 # ThreeBodyDecay structure
 @safetestset "Test of the tbs structure" begin
-    include("threebodymasses.jl")
-    include("threebodyspins.jl")
-    include("threebodyparities.jl")
-    include("invariants.jl")
+    include("test_threebodymasses.jl")
+    include("test_threebodyspins.jl")
+    include("test_threebodyparities.jl")
+    include("test_invariants.jl")
 end
 
 
@@ -18,12 +18,12 @@ include("wignerrotationdispatch.jl")
 # angular functions
 @safetestset "Angular Functions block" begin
     include("circle_relations.jl")
-    include("representaiton_property.jl")
+    include("test_representaiton_property.jl")
     include("check_lineshape_functions.jl")
 end
 
-@safetestset "Permutations" begin include("wigner_permutations.jl") end
-@safetestset "Phases" begin include("phases.jl") end
+@safetestset "Permutations" begin include("test_wigner_permutations.jl") end
+@safetestset "Phases" begin include("test_phases.jl") end
 
 @safetestset "Sum rule" begin include("wigner_angle_sumrules.jl") end
 
@@ -37,17 +37,17 @@ end
 @safetestset "Couplings" begin include("test_couplings.jl") end
 
 @safetestset "LS amplitudes" begin
-    include("ls_amplitude.jl")
-    include("sum_over_polarization.jl")
+    include("test_ls_amplitude.jl")
+    include("test_sum_over_polarization.jl")
 end
 
 # Integrals
 @safetestset "Integrals" begin
-    include("three_body_phase_space_integral.jl")
+    include("test_three_body_phase_space_integral.jl")
 end
 # examples
 @safetestset "Complete example" begin include("test_Xic_decay.jl") end
 
 @safetestset "square dalitz" begin
-    include("transformations_of_squaredalitz.jl")
+    include("test_transformations_of_squaredalitz.jl")
 end
