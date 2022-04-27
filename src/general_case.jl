b5459939badaf46649e4c35e2d7e0364acb40b4b
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2022-04-27 15:55:04 +0200

    update the general case

--- src/general_case.jl
+++ src/general_case.jl
@@ -1,4 +1,31 @@
-function Zksτ(k,two_s,two_τ,two_λs,two_λs′,σs,tbs)
+function Zksτ(k, two_s,two_τ, two_λs, two_λs′, σs, tbs; refζs=(1,2,3,1))
+    i,j = ij_from_k(k)
+    #
+    ms² = tbs.ms^2
+    # 
+    w0 = wr(k,refζs[4],0); cosζ0 = cosζ(w0, σs, ms²)
+    wi = wr(k,refζs[i],i); cosζi = cosζ(wi, σs, ms²)
+    wj = wr(k,refζs[j],j); cosζj = cosζ(wj, σs, ms²)
+    wk = wr(k,refζs[k],k); cosζk = cosζ(wk, σs, ms²)
+    # 
+    cosθ = cosθij(k,σs,ms²)
+    two_λ0 = two_λs[4]
+    two_j0 = tbs.two_js[4]
+    #
+    val = # the constant factors are important for the normalization
+        sqrt(two_j0+1) * wignerd_doublearg_sign(two_j0, two_λ0, two_τ-two_λs′[k], cosζ0, ispositive(w0)) *
+            # 
+            phase(tbs.two_js[k]-two_λs′[k]) * # particle-2 convention
+        sqrt(two_s+1) * wignerd_doublearg(two_s, two_τ, two_λs′[i]-two_λs′[j], cosθ) *
+            phase(tbs.two_js[j]-two_λs′[j]) * # particle-2 convention
+            # 
+            wignerd_doublearg_sign(tbs.two_js[i], two_λs′[i], two_λs[i], cosζi, ispositive(wi)) *
+            wignerd_doublearg_sign(tbs.two_js[j], two_λs′[j], two_λs[j], cosζj, ispositive(wj)) *
+            wignerd_doublearg_sign(tbs.two_js[k], two_λs′[k], two_λs[k], cosζk, ispositive(wk))
+    return val
+end
+
+function Zksτ_old(k,two_s,two_τ,two_λs,two_λs′,σs,tbs)
     (i,j) = (k==1 ? (2,3) : (k==2 ? (3,1) : (1,2)))
     #
     two_Λ = two_λs[4]
@@ -14,4 +41,9 @@ function Zksτ(k,two_s,two_τ,two_λs,two_λs′,σs,tbs)
             phase(2,k,two_λs′[2],two_λs[2]) * wignerd_doublearg(tbs.two_js[2], two_λs′[2], two_λs[2], cosζk2_for2(k,σs,tbs.ms^2)) *
             phase(3,k,two_λs′[3],two_λs[3]) * wignerd_doublearg(tbs.two_js[3], two_λs′[3], two_λs[3], cosζk3_for3(k,σs,tbs.ms^2))
     return val
+    # return cosθhatk1(k,σs,tbs.ms^2)
 end
+
+wignerd_doublearg_sign(j,λ1,λ2, cosθ, ispositive) =
+    (ispositive ? 1 : x"-1"^*(λ1-λ2)) *
+        wignerd_doublearg(j,λ1,λ2, cosθ)
